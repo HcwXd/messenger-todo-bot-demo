bdfc78a2719e7cf48d696e82534bdd66a92e582a
HcwXd <e04dl47878@gmail.com>
2020-01-03 17:08:18 +0800

    feat: Add listTodo function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ module.exports = async function App(context) {
         todos: context.state.todos.concat({ title: todoTitle }),
       });
       await context.sendText(`Add todo: ${todoTitle}.`);
+    } else if (context.event.text === 'list') {
+      if (context.state.todos.length === 0) {
+        await context.sendText(`There's no todo in your list`);
+      } else {
+        context.sendText(
+          `Your Todo:\n${context.state.todos
+            .map(({ title }, idx) => `${idx + 1}. ${title}`)
+            .join('\n')}`
+        );
+      }
     }
   }
 };
