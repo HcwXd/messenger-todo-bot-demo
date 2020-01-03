e2c7b2252301f553412e96b83d40695d097b8df0
HcwXd <e04dl47878@gmail.com>
2020-01-03 17:26:17 +0800

    feat: Add deleteTodo function

--- src/index.js
+++ src/index.js
@@ -6,16 +6,31 @@ module.exports = async function App(context) {
         todos: context.state.todos.concat({ title: todoTitle }),
       });
       await context.sendText(`Add todo: ${todoTitle}.`);
-    } else if (context.event.text === 'list') {
+    } else if (context.event.text === '/l') {
       if (context.state.todos.length === 0) {
         await context.sendText(`There's no todo in your list`);
       } else {
-        context.sendText(
-          `Your Todo:\n${context.state.todos
-            .map(({ title }, idx) => `${idx + 1}. ${title}`)
-            .join('\n')}`
+        await context.sendGenericTemplate(
+          context.state.todos.map(({ title }) => {
+            return {
+              title: title,
+              buttons: [
+                {
+                  type: 'postback',
+                  title: 'Delete',
+                  payload: title,
+                },
+              ],
+            };
+          })
         );
       }
     }
+  } else if (context.event.isPostback) {
+    const targetTodo = context.event.postback.payload;
+    context.setState({
+      todos: context.state.todos.filter(({ title }) => title !== targetTodo),
+    });
+    await context.sendText(`Delete todo: ${targetTodo}.`);
   }
 };
