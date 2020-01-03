01cee9825f6cbcc226f8195dbcd0a8adeb71b777
HcwXd <e04dl47878@gmail.com>
2020-01-03 17:54:41 +0800

    feat: Refactor with bottender/router

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
-module.exports = async function App(context) {
-  if (context.event.isText) {
-    if (context.event.text.startsWith('/a ')) {
-      const todoTitle = context.event.text.slice(3);
-      context.setState({
-        todos: context.state.todos.concat({ title: todoTitle }),
-      });
-      await context.sendText(`Add todo: ${todoTitle}.`);
-    } else if (context.event.text === '/l') {
-      if (context.state.todos.length === 0) {
-        await context.sendText(`There's no todo in your list`);
-      } else {
-        await context.sendGenericTemplate(
-          context.state.todos.map(({ title }) => {
-            return {
-              title: title,
-              buttons: [
-                {
-                  type: 'postback',
-                  title: 'Delete',
-                  payload: title,
-                },
-              ],
-            };
-          })
-        );
-      }
-    }
-  } else if (context.event.isPostback) {
-    const targetTodo = context.event.postback.payload;
-    context.setState({
-      todos: context.state.todos.filter(({ title }) => title !== targetTodo),
-    });
-    await context.sendText(`Delete todo: ${targetTodo}.`);
+const { router, text, route } = require('bottender/router');
+
+async function addTodo(context) {
+  const todoTitle = context.event.text.slice(3);
+  context.setState({
+    todos: context.state.todos.concat({ title: todoTitle }),
+  });
+  await context.sendText(`Add todo: ${todoTitle}`);
+}
+
+async function listTodo(context) {
+  if (context.state.todos.length === 0) {
+    await context.sendText(`There's no todo in your list`);
+  } else {
+    await context.sendGenericTemplate(
+      context.state.todos.map(({ title }) => {
+        return {
+          title: title,
+          buttons: [
+            {
+              type: 'postback',
+              title: 'Delete',
+              payload: title,
+            },
+          ],
+        };
+      })
+    );
   }
+}
+
+async function deleteTodo(context) {
+  const targetTodo = context.event.postback.payload;
+  context.setState({
+    todos: context.state.todos.filter(({ title }) => title !== targetTodo),
+  });
+  await context.sendText(`Delete todo: ${targetTodo}`);
+}
+
+module.exports = async function App(context) {
+  return router([
+    text(/^\/a/, addTodo),
+    text('/l', listTodo),
+    route(
+      context => context.event.isPostback && context.event.postback.title === 'Delete',
+      deleteTodo
+    ),
+  ]);
 };
