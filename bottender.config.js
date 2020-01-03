a079741219058f5f95094ee3b1ff87e2c8f79d2f
HcwXd <e04dl47878@gmail.com>
2020-01-03 17:03:53 +0800

    feat: Add addTodo function

--- bottender.config.js
+++ bottender.config.js
@@ -20,7 +20,7 @@ module.exports = {
       },
     },
   },
-  initialState: {},
+  initialState: { todos: [] },
   channels: {
     messenger: {
       enabled: true,
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
+  if (context.event.isText) {
+    if (context.event.text.startsWith('/a ')) {
+      const todoTitle = context.event.text.slice(3);
+      context.setState({
+        todos: context.state.todos.concat({ title: todoTitle }),
+      });
+      await context.sendText(`Add todo: ${todoTitle}.`);
+    }
+  }
 };
